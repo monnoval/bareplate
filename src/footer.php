a209e2f17d861f7746910207a720268afb7ada08
Mon Noval <mon@monnoval.com>
2015-12-20 23:04:59 +0800

    Fix footer

--- src/footer.php
+++ src/footer.php
@@ -14,20 +14,13 @@
 
 	<footer class="footer site__footer" role="contentinfo">
 
-		<div class="site--wrapper footer__site--wrapper">
-			<div class="footer__menu js__footer__menu">
-				<nav id="footer-navigation" class="footer__menu-nav" role="navigation">
-					<?php /*/ ?>
-					<?php wp_nav_menu( array(
-						'theme_location' => 'footer',
-						'menu_class' => 'menu__ul',
-						) ); ?>
-					<?php /*/ ?>
-				</nav>
-			</div>
-
+		<div class="site--wrapper">
 			<div class="footer__legal">
-				<?php _e( 'Copyright &copy; 2015 Bareplate Theme. Powered by WordPress and Bareplate.', 'bareplate' ) ?>
+				<?php
+					_e( 'Copyright &copy; ', 'bareplate' );
+					echo date("Y");
+					_e( ' Bareplate. ', 'bareplate' );
+					_e( 'Powered by WordPress and Bareplate.', 'bareplate' );  ?>
 			</div>
 		</div>
 
@@ -36,10 +29,6 @@
 </div><!-- .site -->
 
 <?php wp_footer(); ?>
-
-<!--[if lte IE 8]>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
-<![endif]-->
 
 </body>
 </html>
